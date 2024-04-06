98eb46c007b99387e327e51bf6fe5de993f6529e
devil <143690452+devil945727@users.noreply.github.com>
2024-04-07 01:17:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,8 @@ function secondsToMinutesSeconds(seconds) {
 }
 async function getSongs(folder) {
     currfolder=folder;
-    let a = await fetch('/${folder}/`);
+    // let a = await fetch('/${folder}/`);
+    let a = await fetch(`/${folder}/`);
     let response = await a.text();
     // console.log(response);
     let div=document.createElement("div");
